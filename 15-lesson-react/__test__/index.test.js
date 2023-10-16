405630b26e1b0936bb335eb72c91c39f68241a27
SuxrobAbdumalikov <suxrobabdumalikov99@gmail.com>
2023-10-16 12:22:20 +0500

    testing and json-server

---
+++ 15-lesson-react/__test__/index.test.js
@@ -0,0 +1,5 @@
+const { divide, multiple, add, differ } = require("../index");
+
+test("we are checking divide function", () => {
+  expect(add(1, 4)).toBe(5);
+});
---
+++ 15-lesson-react/index.js
@@ -0,0 +1,17 @@
+function multiple(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function differ(a, b) {
+  return a - b;
+}
+
+module.exports = { multiple, divide, add, differ };
