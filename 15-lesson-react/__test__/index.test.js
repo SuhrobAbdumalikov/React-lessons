1184f2346d8fa2d7c13c5cf936a5e8b11a61931e
SuxrobAbdumalikov <suxrobabdumalikov99@gmail.com>
2023-10-16 12:49:18 +0500

    testing and json-server

--- 15-lesson-react/__test__/index.test.js
+++ 15-lesson-react/__test__/index.test.js
@@ -1,5 +1,22 @@
 const { divide, multiple, add, differ } = require("../index");
 
-test("we are checking divide function", () => {
-  expect(add(1, 4)).toBe(5);
+describe("#add", () => {
+  test("we are checking divide function", () => {
+    expect(add(1, 4)).toBe(5);
+  });
+});
+describe("#differ", () => {
+  test("we are checking divide function", () => {
+    expect(differ(1, 4)).toBe(-3);
+  });
+});
+describe("#multiple", () => {
+  test("we are checking divide function", () => {
+    expect(multiple(1, 4)).toBe(4);
+  });
+});
+describe("#divide", () => {
+  test("we are checking divide function", () => {
+    expect(divide(4, 2)).toBe(2);
+  });
 });
